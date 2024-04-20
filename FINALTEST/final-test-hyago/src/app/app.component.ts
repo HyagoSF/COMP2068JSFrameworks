f61f476968cad07a7462c9af6db83dc6235a5e9d
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2024-04-20 15:09:27 -0400

    Task 3 completed

--- FINALTEST/final-test-hyago/src/app/app.component.ts
+++ FINALTEST/final-test-hyago/src/app/app.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    StudentsComponent,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'final-test-hyago';
--- FINALTEST/final-test-hyago/src/app/students/students.component.ts
+++ FINALTEST/final-test-hyago/src/app/students/students.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../services/student.service';
+import Student from './models/student';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './students.component.html',
-  styleUrl: './students.component.css'
+  styleUrl: './students.component.css',
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit {
+  students: Student[] = [];
+  selectedStudent: Student | null = { id: 200536449, name: 'Hyago' };
+  isFormOpen: boolean = false;
 
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(): void {
+    this.studentService
+      .getStudents()
+      .subscribe((students) => (this.students = students));
+  }
+
+  openCreateForm() {
+    this.selectedStudent = null;
+    this.isFormOpen = true;
+  }
+
+  openUpdateForm(student: Student) {
+    this.selectedStudent = student;
+    this.isFormOpen = true;
+  }
+
+  deleteStudent(id: number) {
+    this.studentService.deleteStudent(id).subscribe(() => this.getStudents());
+  }
+
+  onSubmit() {
+    if (this.selectedStudent) {
+      this.updateStudent();
+    } else {
+      this.createStudent();
+    }
+  }
+
+  createStudent() {
+    this.studentService
+      .createStudent(this.selectedStudent as Student)
+      .subscribe(() => {
+        this.getStudents();
+        this.isFormOpen = false;
+        this.selectedStudent = null;
+      });
+  }
+
+  updateStudent() {
+    this.studentService
+      .updateStudent(
+        this.selectedStudent?.id as number,
+        this.selectedStudent as Student
+      )
+      .subscribe(() => {
+        this.getStudents();
+        this.isFormOpen = false;
+        this.selectedStudent = null;
+      });
+  }
 }
